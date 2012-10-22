b7e1f522a788fd6334d4bf679eb7e6ceed33b5f0
rolo2012 <rolando.corratge@etecsa.cu>
2012-10-22 18:34:17 +0000

    dd

--- application/views/app_shell.js.php
+++
@@ -1,4 +0,0 @@
-<?php
-$app_shell=file_get_contents('assets/js/appshell_extensions.js');
-header("Content-Type: application/javascript");
-die(str_replace('native', '//native', $app_shell));
--- assets/public/js/fixes.js
+++
@@ -1,9 +0,0 @@
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global $,document */
-
-$(function () {
-    "use strict";
-    $(document).on('click', "a[href='#']", function (e) {
-        e.preventDefault();
-    });
-});
