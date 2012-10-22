ff68f06601e8fd69183fb71e89bdd0e6ee9bfb2d
rolo2012 <rolando.corratge@etecsa.cu>
2012-10-22 18:42:33 +0000

    ertwe

--- application/views/path_config_js.php
+++
@@ -1,8 +0,0 @@
-var NAT;
-if (!NAT) {
-    NAT = {};
-}  
-NAT.BASE_URL = "<?=base_url();?>";
-NAT.SITE_URL = "<?=site_url();?>"
-NAT.AJAX_URL = "<?=site_url('ajax_api/')?>"
-NAT.MAIN_CC_URL = "<?=site_url('main/');?>";
